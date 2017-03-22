46d00f41166231336eb64733bd1fac9f108e3ceb
timdows <tim@inexpro.nl>
2017-03-22 23:38:50 +0100

    Implement clientValue

--- Controllers/Heater/HeaterClientModel.cs
+++ Controllers/Heater/HeaterClientModel.cs
@@ -1,6 +1,7 @@
 ﻿using HouseDB.Data;
 using HouseDB.Data.Models;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -10,6 +11,7 @@ namespace HouseDB.Controllers.Heater
 	{
 		public HeaterMeterGroup HeaterMeterGroup { get; set; }
 		public List<HeaterValue> HeaterValues { get; set; }
+		public List<HeaterClientValue> HeaterClientValues { get; set; } = new List<HeaterClientValue>();
 
 		public void Load(DataContext dataContext, long heaterMeterGroupID)
 		{
@@ -26,13 +28,41 @@ namespace HouseDB.Controllers.Heater
 				.Where(a_item => a_item.HeaterMeter.HeaterMeterGroup.ID == HeaterMeterGroup.ID)
 				.OrderBy(a_item => a_item.Date)
 				.ToList();
+
+			var sum = 0;
+			var lastValue = 0;
+			foreach(var heaterValue in HeaterValues)
+			{
+				var difference = 0;
+				if (lastValue != 0 && heaterValue.Value >= lastValue)
+				{
+					difference = heaterValue.Value - lastValue;
+					lastValue = heaterValue.Value;
+				}
+				else
+				{
+					difference = heaterValue.Value;
+					lastValue = heaterValue.Value;
+				}
+
+				sum += difference;
+
+				HeaterClientValues.Add(new HeaterClientValue
+				{
+					Date = heaterValue.Date,
+					PureValue = heaterValue.Value,
+					SumValue = sum,
+					DifferenceValue = difference
+				});
+			}
 		}
 	}
 
-	//public class HeaterClientValue
-	//{
-	//	public DateTime Date { get; set; }
-	//	public int Value { get; set; }
-
-	//}
+	public class HeaterClientValue
+	{
+		public DateTime Date { get; set; }
+		public int PureValue { get; set; }
+		public int SumValue { get; set; }
+		public int DifferenceValue { get; set; }
+	}
 }
