d15167a57d795170aab52ef4851ccdf9b0e59823
timdows <tim@inexpro.nl>
2017-03-23 23:59:01 +0100

    Extra settings

--- Settings/VeraSettings.cs
+++ Settings/VeraSettings.cs
@@ -5,6 +5,7 @@
 		public string VeraIpAddress { get; set; }
 		public string Username { get; set; }
 		public string Password { get; set; }
-		public string DataMineDirectoryPath { get; set; }
+		public string DataMineDirectoryPath { get; set; } // DataMine database on the vera3
+		public string ExportPathOnPi { get; set; } // Working path on the Raspberry Pi
 	}
 }
