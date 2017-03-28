0ebbff414293361b50f273e80ca306456b4663f7
timdows <tim@inexpro.nl>
2017-03-29 00:17:36 +0200

    Fixes on getting the value

--- Controllers/SevenSegment/PowerImportValueClientModel.cs
+++ Controllers/SevenSegment/PowerImportValueClientModel.cs
@@ -5,6 +5,7 @@
 		public string Label { get; set; }
 		public decimal Min { get; set; }
 		public decimal Max { get; set; }
+
 		public string Type
 		{
 			get
@@ -16,5 +17,9 @@
 				return "Low";
 			}
 		}
+
+		public bool Valid => Max != -9999999999 && Min != 9999999999;
+
+		public decimal? ValidMax => Valid ? (decimal?)Max : null;
 	}
 }
