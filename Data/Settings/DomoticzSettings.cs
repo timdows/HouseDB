02bb561cebcca0b380504183b63dd4aae92e9bba
Tim <tim@inexpro.nl>
2017-06-24 10:53:01 +0200

    Add port to domoticz settings

--- Data/Settings/DomoticzSettings.cs
+++ Data/Settings/DomoticzSettings.cs
@@ -3,6 +3,7 @@
 	public class DomoticzSettings
     {
 		public string Host { get; set; }
+		public int Port { get; set; }
 		public int WattIdx { get; set; } // IDX to get the current Watt from smart meter from
 	}
 }
