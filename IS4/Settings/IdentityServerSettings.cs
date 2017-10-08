78a0b08f895507304a2604ff4ba684ae24be5833
Timdows <tim@inexpro.nl>
2017-10-08 16:42:22 +0200

    Create IdentityServer4 with json configuration

---
+++ IS4/Settings/IdentityServerSettings.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace IS4.Settings
+{
+	public class IdentityServerSettings
+    {
+		public List<IdentityServerApi> IdentityServerApis { get; set; }
+		public CertificateSettings CertificateSettings { get; set; }
+	}
+
+	public class IdentityServerApi
+	{
+		public string Name { get; set; }
+		public string DisplayName { get; set; }
+		public List<IdentityServerClient> IdentityServerClients { get; set; }
+	}
+
+	public class IdentityServerClient
+	{
+		public string ClientID { get; set; }
+		public string Secret { get; set; }
+	}
+
+	public class CertificateSettings
+	{
+		public string FileName { get; set; }
+		public string Password { get; set; }
+	}
+}
