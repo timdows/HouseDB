fd36ef16c53afb89e0054451efc87bcd547904c9
Timdows <tim@inexpro.nl>
2017-11-07 23:14:14 +0100

    Add area usage test method

---
+++ Api/Controllers/AreaUsage/AreaUsageController.cs
@@ -0,0 +1,36 @@
+﻿using HouseDB.Controllers;
+using HouseDB.Controllers.Exporter;
+using HouseDB.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Api.Controllers.AreaUsage
+{
+	[Route("[controller]/[action]")]
+	public class AreaUsageController : HouseDBController
+	{
+		private readonly IMemoryCache _memoryCache;
+
+		public AreaUsageController(
+			DataContext dataContext,
+			IMemoryCache memoryCache
+			) : base(dataContext)
+		{
+			_memoryCache = memoryCache;
+		}
+
+		public JsonResult Test()
+		{
+			// Get cached objects
+			var domoticzValuesForCachingClientModelCache = _memoryCache.Get(nameof(DomoticzValuesForCachingClientModel));
+
+			if (domoticzValuesForCachingClientModelCache != null)
+			{
+				var domoticzValuesForCachingClientModel = domoticzValuesForCachingClientModelCache as DomoticzValuesForCachingClientModel;
+				return Json(domoticzValuesForCachingClientModel);
+			}
+
+			return Json(0);
+		}
+	}
+}
