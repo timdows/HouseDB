101bb51bd1e47fe2693527be1f0e929d6d0efeef
Timdows <tim@inexpro.nl>
2017-11-11 12:00:12 +0100

    Create week overview

--- Api/Controllers/AreaUsage/AreaUsageController.cs
+++ Api/Controllers/AreaUsage/AreaUsageController.cs
@@ -1,8 +1,12 @@
 ﻿using HouseDB.Controllers;
 using HouseDB.Controllers.Exporter;
 using HouseDB.Data;
+using HouseDB.Data.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Api.Controllers.AreaUsage
 {
@@ -10,6 +14,7 @@ namespace Api.Controllers.AreaUsage
 	public class AreaUsageController : HouseDBController
 	{
 		private readonly IMemoryCache _memoryCache;
+		private readonly List<Device> _devices;
 
 		public AreaUsageController(
 			DataContext dataContext,
@@ -17,21 +22,56 @@ namespace Api.Controllers.AreaUsage
 			) : base(dataContext)
 		{
 			_memoryCache = memoryCache;
+			_devices = _dataContext.Devices
+				.Where(a_item => a_item.IsForKwhImport &&
+								 (a_item.DomoticzWattIdx != 0 || a_item.DomoticzKwhIdx != 0))
+				.ToList();
 		}
 
 		[HttpGet]
-		public JsonResult Test()
+		public JsonResult WeekOverview()
 		{
 			// Get cached objects
 			var domoticzValuesForCachingClientModelCache = _memoryCache.Get(nameof(DomoticzValuesForCachingClientModel));
 
+			DomoticzValuesForCachingClientModel domoticzValuesForCachingClientModel;
 			if (domoticzValuesForCachingClientModelCache != null)
 			{
-				var domoticzValuesForCachingClientModel = domoticzValuesForCachingClientModelCache as DomoticzValuesForCachingClientModel;
-				return Json(domoticzValuesForCachingClientModel);
+				domoticzValuesForCachingClientModel = domoticzValuesForCachingClientModelCache as DomoticzValuesForCachingClientModel;
 			}
 
-			return Json(0);
+			var oneWeekAgo = DateTime.Today.AddDays(-7);
+			var weekDeviceOverviews = new List<WeekDeviceOverview>();
+
+			foreach (var device in _devices)
+			{
+				var weekDeviceOverview = new WeekDeviceOverview
+				{
+					Device = device
+				};
+
+				// Get last weeks values from database
+				var dbValues = _dataContext.KwhDateUsages
+					.Where(a_item => a_item.DeviceID == device.ID &&
+									 a_item.Date > oneWeekAgo)
+					.OrderBy(a_item => a_item.Date)
+					.ToList();
+
+				foreach (var value in dbValues)
+				{
+					weekDeviceOverview.Values.Add(value.Usage);
+				}
+
+				weekDeviceOverviews.Add(weekDeviceOverview);
+			}
+
+			return Json(weekDeviceOverviews);
 		}
+	}
+
+	public class WeekDeviceOverview
+	{
+		public Device Device { get; set; }
+		public List<decimal> Values { get; set; } = new List<decimal>();
 	}
 }
