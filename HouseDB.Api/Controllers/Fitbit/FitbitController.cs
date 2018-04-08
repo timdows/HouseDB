85a02dd81de56a56ee46a93608a2f694938a0cba
Timdows <tim@inexpro.nl>
2018-04-08 14:39:30 +0200

    Save activity steps in database

--- HouseDB.Api/Controllers/Fitbit/FitbitController.cs
+++ HouseDB.Api/Controllers/Fitbit/FitbitController.cs
@@ -1,13 +1,10 @@
 ﻿using HouseDB.Api.Data;
-using HouseDB.Api.Data.Fitbit;
 using HouseDB.Api.Data.FitbitResponse;
 using HouseDB.Api.Data.Models;
 using HouseDB.Core.Settings;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
-using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -27,16 +24,48 @@ namespace HouseDB.Api.Controllers.Fitbit
 		[HttpGet]
 		public async Task<IActionResult> GetActivitySteps(string clientId)
 		{
+			var existingFitbitActivitySteps = _dataContext.FitbitActivitySteps
+				.Where(item => item.FitbitClientDetail.ClientId == clientId)
+				.ToList();
+
+			var fitbitClientDetail = _dataContext.FitbitClientDetails
+				.Single(item => item.ClientId == clientId);
+
 			var token = await FitbitHelper.GetAccessToken(_dataContext, _fitbitSettings, clientId);
-			var response = await FitbitHelper.GetResponseFromApi(token, "https://api.fitbit.com/1/user/-/activities/steps/date/today/1y.json");
 
-			var jToken = response["activities-steps"];
-			var activitySteps = FitbitHelper.GetResponseList<ActivityStep>(jToken);
+			if (existingFitbitActivitySteps.Any(item => item.Date == DateTime.Today.AddDays(-1)))
+			{
+				// Only get today's steps
+				var response = await FitbitHelper.GetResponseFromApi(token, "https://api.fitbit.com/1/user/-/activities/steps/date/today/1d.json");
+				var jToken = response["activities-steps"];
+				var activitySteps = FitbitHelper.GetResponseList<ActivityStep>(jToken);
+				var fitbitActivitySteps = FitbitActivityStep.GetFitbitActivitySteps(activitySteps, fitbitClientDetail);
+				existingFitbitActivitySteps.AddRange(fitbitActivitySteps);
+				return Json(existingFitbitActivitySteps.OrderByDescending(item => item.Date));
+			}
+			else
+			{
+				var response = await FitbitHelper.GetResponseFromApi(token, "https://api.fitbit.com/1/user/-/activities/steps/date/today/1y.json");
 
-			return Json(activitySteps);
+				var jToken = response["activities-steps"];
+				var activitySteps = FitbitHelper.GetResponseList<ActivityStep>(jToken);
+				var fitbitActivitySteps = FitbitActivityStep.GetFitbitActivitySteps(activitySteps, fitbitClientDetail);
+
+				var existingDates = existingFitbitActivitySteps.Select(item => item.Date).ToList();
+
+				foreach (var fitbitActivityStep in fitbitActivitySteps)
+				{
+					if (existingDates.Contains(fitbitActivityStep.Date)) continue;
+					if (fitbitActivityStep.Steps == 0 || fitbitActivityStep.Date == DateTime.Today) continue;
+
+					_dataContext.FitbitActivitySteps.Add(fitbitActivityStep);
+				}
+
+				await _dataContext.SaveChangesAsync();
+
+				return Json(fitbitActivitySteps.OrderByDescending(item => item.Date));
+			}
 		}
-
-		
 
 		[HttpGet]
 		public async Task<IActionResult> GetActivityDistance(string clientId)
--- HouseDB.Api/Migrations/20180408120542_AddClientDetails.Designer.cs
+++ HouseDB.Api/Migrations/20180408120542_AddClientDetails.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace HouseDB.Api.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20180408120542_AddClientDetails")]
+    partial class AddClientDetails
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
