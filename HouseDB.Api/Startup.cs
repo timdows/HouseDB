d7afecc0762332b712e654eddf3525a5145825bb
Timdows <tim@inexpro.nl>
2018-04-28 22:08:30 +0200

    Cahce issues?

--- HouseDB.Api/Startup.cs
+++ HouseDB.Api/Startup.cs
@@ -40,6 +40,7 @@ namespace HouseDB.Api
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddMemoryCache();
 			services.AddMvc()
 				.AddJsonOptions(options =>
 				{
