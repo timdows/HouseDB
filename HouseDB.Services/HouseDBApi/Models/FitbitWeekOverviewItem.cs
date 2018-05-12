2d2eab01280bc657896cc9cac64bb90983a3185f
Timdows <tim@inexpro.nl>
2018-05-12 19:56:52 +0200

    Update services

--- HouseDB.Services/HouseDBApi/Models/FitbitWeekOverviewItem.cs
+++ HouseDB.Services/HouseDBApi/Models/FitbitWeekOverviewItem.cs
@@ -22,11 +22,12 @@ namespace HouseDB.Services.HouseDBApi.Models
         /// <summary>
         /// Initializes a new instance of the FitbitWeekOverviewItem class.
         /// </summary>
-        public FitbitWeekOverviewItem(System.DateTime? date = default(System.DateTime?), int? steps = default(int?), double? kiloMeters = default(double?))
+        public FitbitWeekOverviewItem(System.DateTime? date = default(System.DateTime?), int? steps = default(int?), double? kiloMeters = default(double?), string displayText = default(string))
         {
             Date = date;
             Steps = steps;
             KiloMeters = kiloMeters;
+            DisplayText = displayText;
             CustomInit();
         }
 
@@ -49,6 +50,11 @@ namespace HouseDB.Services.HouseDBApi.Models
         /// </summary>
         [JsonProperty(PropertyName = "kiloMeters")]
         public double? KiloMeters { get; set; }
+
+        /// <summary>
+        /// </summary>
+        [JsonProperty(PropertyName = "displayText")]
+        public string DisplayText { get; set; }
 
     }
 }
