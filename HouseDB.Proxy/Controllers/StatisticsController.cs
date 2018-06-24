547fcc5f1ef0379756fbc6b8d00b1e41154cf53a
Timdows <tim@inexpro.nl>
2018-06-24 18:43:07 +0200

    Update proxy

--- HouseDB.Proxy/Controllers/StatisticsController.cs
+++ HouseDB.Proxy/Controllers/StatisticsController.cs
@@ -69,5 +69,17 @@ namespace HouseDB.Proxy.Controllers
 				return Json(response);
 			}
 		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetP1WeekUsage(int amountOfWeeks)
+		{
+			using (var api = new HouseDBAPI(new Uri(_houseDBSettings.ApiUrl)))
+			{
+				var token = await _jwtTokenManager.GetToken(_houseDBSettings);
+				api.HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+				var response = await api.StatisticsGetP1WeekUsageGetAsync(amountOfWeeks);
+				return Json(response);
+			}
+		}
 	}
 }
