17cb5eaf888adb4cbb75c101acb78a7a3f410bcb
timdows <tim@inexpro.nl>
2020-03-16 12:14:08 +0100

    Add P1Consumption migration

---
+++ HouseDB.Data/Migrations/20200316111319_P1Consumption.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HouseDB.Data.Migrations
+{
+    public partial class P1Consumption : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "P1Consumptions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DateTimeDeleted = table.Column<DateTime>(nullable: true),
+                    DeletedByIdentityServerUserId = table.Column<int>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    DayUsage = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_P1Consumptions", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "P1Consumptions");
+        }
+    }
+}
