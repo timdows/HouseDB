fe35895f4f47db0e661cee35609dfc66e7e3e631
timdows <tim@inexpro.nl>
2020-03-17 20:31:52 +0100

    No formatted value

--- HouseDB.Core/Models/DeviceKwhUsage.cs
+++ HouseDB.Core/Models/DeviceKwhUsage.cs
@@ -5,18 +5,9 @@
         public string Name { get; set; }
         
         public decimal Today { get; set; }
-        public string TodayFormatted => $"{decimal.Round(Today, 2)}";
-
         public decimal ThisWeek { get; set; }
-        public string ThisWeekFormatted => $"{decimal.Round(ThisWeek, 2)}";
-
         public decimal LastWeek { get; set; }
-        public string LastWeekFormatted => $"{decimal.Round(LastWeek, 2)}";
-
         public decimal ThisMonth { get; set; }
-        public string ThisMonthFormatted => $"{decimal.Round(ThisMonth, 2)}";
-
         public decimal LastMonth { get; set; }
-        public string LastMonthFormatted => $"{decimal.Round(LastMonth, 2)}";
     }
 }
